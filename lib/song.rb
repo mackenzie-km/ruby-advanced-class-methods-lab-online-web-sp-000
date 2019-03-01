307077d77c9d1bf00626a2a6e107ba8f6f9705f3
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-01 21:05:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,26 @@
-class Song
+   require "pry"
+   class Song
+
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def save
+    self.class.all << self
+  end 
+  
+  def self.create
+    new_song = self.new
+    new_song.save
+    new_song
+  end 
 
+ def self.new_by_name(inputted_name)
+new_song = self.new
+new_song.name = "#{inputted_name}"
+new_song
+end 
+  
   def self.all
     @@all
   end
-
-  def save
-    self.class.all << self
-  end
-
 end
