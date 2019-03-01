042af3ecb16952025eeedcbc0f24a6f8688c4e38
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-01 21:53:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,8 @@ self.all.sort_by{|song| song.name}
 end 
 
 def self.new_from_filename(inputted_string)
-  inputted_string.delete ".mp3"
-  splitted = inputted_string.split(" - ")
+  splitted = inputted_string.delete ".mp3"
+  splitted = splitted.split(" - ")
   inputted_artist = splitted[0]
   inputted_song = splitted[1]
   added_song = self.new_by_name(inputted_song)
